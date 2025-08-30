2cafcda03f78856bbae616b927248720177f232c
MJ <alswns8495@naver.com>
2025-08-30 15:29:01 +0900

    🎉 onePercent 프로젝트 생성 및 FSD 기반 폴더 생성 (Vite+React+TS with FSD)

---
+++ src/app/App.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import Session from "../components/Session";
+import Footer from "../components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Session />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
